1f2ac6f13ef2549ed4bc86ed1c185dac5bd33405
AurelianAM <117901747+AurelianAM@users.noreply.github.com>
2023-07-19 22:48:28 +0300

    Added first files

---
+++ testCalculator.py
@@ -0,0 +1,44 @@
+import unittest
+from calculator import Calculator
+
+class testSumFromNumbers(unittest.TestCase):
+    # def test_sumFromNumbers(self):
+    #     calc = Calculator()
+    #     result = calc.sumFromNumbers(2, 3)
+    #     expected = 5
+    #     self.assertEqual(result, expected)
+    #     self.assertNotEqual(result, expected)
+    #     self.assertGreaterEqual(result, expected)
+
+    def test_isCorrect(self):
+        calc = Calculator()
+        result = calc.sumFromNumbers(2, 3)
+        expected = 5
+        self.assertEqual(result, expected)
+    
+    def test_isNegative(self):
+        calc = Calculator()
+        result = calc.sumFromNumbers(-4, 1)
+        expected = 0
+        self.assertLess(result, expected)
+    
+    def test_isGreater(self):
+        calc = Calculator()
+        result = calc.sumFromNumbers(2, 3)
+        expected = 5
+        self.assertGreaterEqual(result, expected)
+        
+
+def suite():
+    # gather all tests in a test suite
+
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(unittest.makeSuite(testSumFromNumbers))
+    return test_suite
+
+mySuite = suite()
+
+# define Runner
+
+runner = unittest.TextTestRunner()
+runner.run(mySuite)
